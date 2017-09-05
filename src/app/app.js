5532e32442bb65ad3fbfcb188ff57ea292950b03
davidrohof <30686930+davidrohof@users.noreply.github.com>
2017-09-05 12:07:23 +0200

    root src

---
+++ src/app/app.js
@@ -0,0 +1,51 @@
+(function() {
+  'use strict';
+  angular.module('formioAppBasic', [
+    'ngSanitize',
+    'ui.router',
+    'ui.bootstrap',
+    'formio',
+    'ngFormioHelper'
+  ])
+  .config([
+    '$locationProvider',
+    '$stateProvider',
+    '$urlRouterProvider',
+    'FormioProvider',
+    'FormioAuthProvider',
+    'FormioResourceProvider',
+    'AppConfig',
+    '$injector',
+    function(
+      $locationProvider,
+      $stateProvider,
+      $urlRouterProvider,
+      FormioProvider,
+      FormioAuthProvider,
+      FormioResourceProvider,
+      AppConfig,
+      $injector
+    ) {
+      $locationProvider.hashPrefix('');
+      FormioProvider.setAppUrl(AppConfig.appUrl);
+      FormioProvider.setBaseUrl(AppConfig.apiUrl);
+      FormioAuthProvider.setForceAuth(true);
+      FormioAuthProvider.setStates('auth.login', 'seddelIndex');
+      FormioAuthProvider.register('login', 'user', 'login');
+      FormioAuthProvider.register('register', 'user', 'register');
+
+      $stateProvider
+        .state('home', {
+          url: '/',
+          templateUrl: 'views/home.html'
+        });
+
+      // Register all of the resources.
+      angular.forEach(AppConfig.resources, function(resource, name) {
+        FormioResourceProvider.register(name, resource.form, $injector.get(resource.resource + 'Provider'));
+      });
+
+      $urlRouterProvider.otherwise('/seddel');
+    }
+  ]);
+})();
